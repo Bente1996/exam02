a2f25ea2903040fd007190b0bd275d1fd50125e7
Bente1996 <bentedekoning@gmail.com>
2025-04-30 15:47:51 +0200

    comments

--- anagram.c
+++ anagram.c
@@ -88,3 +88,27 @@ int	main(int argc, char **argv)
 
 // memcpy: vergelijk dest met one
 // geen memcpy: check of alles in two speciaal karakter is
+
+// int		is_anagram(char *a, char *b)
+// {
+// 	int	tab[127] = {0};
+// 	int	count = 0;
+
+// 	for	(int i = 0; a[i] != '\0'; i += 1)
+// 		tab[(int)a[i]] += 1;
+// 	for	(int i = 0; b[i] != '\0'; i += 1)
+// 		tab[(int)b[i]] += 1;
+// 	while (i < 126)
+// 		if (tab[i++] != 0)
+// 			break ;
+// 	return (i < 126 ? 0 : 1);
+//}
+
+// int 	main(void)
+// {
+// 		char 	*a = "cinema";
+// 		char	*b = "iceman";
+
+// 		printf("%s\n", (is_anagram(a, b) ? "Anagram" : "Nada"));
+// 		return (0);
+// }
